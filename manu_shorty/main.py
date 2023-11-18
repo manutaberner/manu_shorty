0a35689ec5bef60d941e40866b2afc318b7eb10c
magnumelio <manutabernerllorca@gmail.com>
2023-11-18 01:45:00 +0100

    third build

--- manu_shorty/main.py
+++ manu_shorty/main.py
@@ -15,9 +15,11 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 models.Base.metadata.create_all(bind=engine)
+print(os.environ.get("PORT"))
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Use port 8000 if $PORT is not set
+    port = int(os.environ.get("PORT", 5000))  # Use port 8000 if $PORT is not set
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 def get_db():
     db = SessionLocal()
     try:
